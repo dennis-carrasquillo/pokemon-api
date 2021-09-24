cdc3b558f7971793487b7b83e2dc50eb3f807f71
Dennis Carrasquillo <spycydennis@gmail.com>
2021-09-23 23:03:50 -0400

    Added list-pokemon component, pokemon model, and a navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ListPokemonComponent
   ],
   imports: [
     BrowserModule,
