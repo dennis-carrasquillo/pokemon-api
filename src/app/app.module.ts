8be20f4ddb2edac664433ce2b1a6dd4a91648582
Dennis Carrasquillo <spycydennis@gmail.com>
2021-09-24 18:58:16 -0400

    Added error handling logic to get pokemon service and added the HttpClientModule to the app.module"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/get-pokemon.service.ts
+++ src/app/services/get-pokemon.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/internal/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const pokemonEndpoint = 'https://pokeapi.co/api/v2/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetPokemonService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  private handleError(error: HttpErrorResponse): any {
+    
+    if(error.error instanceof ErrorEvent){
+      console.error('An error occurred: ', error.error.message);
+    }
+    else {
+      console.error(
+        `Backend returned code ${error.status}`, +
+        `body was: ${error.error}`
+      );
+
+      return throwError( 'Something bad happened; please try again later.');
+    }
+
+  }
+    
+
 }
